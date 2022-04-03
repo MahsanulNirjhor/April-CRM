fadd4f7eee50a42d4ac366531256c1e0a5260a37
S M Mahsanul Islam Nirjhor <mahsanulnirjhor@gmail.com>
2022-04-03 16:09:46 +0600

    Finished upto 17:11 min

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import TicketPage from "./pages/TicketPage";
+import Nav from "./components/Nav";
 
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  )
+    return (
+        <div className="app">
+            <Nav />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/ticket" element={<TicketPage />} />
+                    <Route path="/ticket/:id" element={<TicketPage editMode = {true}/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
